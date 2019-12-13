7e2f760f3c79a39950655d59764a98e683fcc902
Frank Valdez <fvaldez421@gmail.com>
2019-12-13 00:21:04 -0800

    added base components

---
+++ src/components/Modal.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+export class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        This is a modal
+      </div>
+    )
+  }
+}
