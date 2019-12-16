fb2c69cf1e25a1f37fd99049567dcd8516503e61
Frank Valdez <fvaldez421@gmail.com>
2019-12-16 02:30:17 -0800

    added in mock requests and added props and state

--- src/pages/ResultsPage/index.js
+++ src/pages/ResultsPage/index.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+
 import { GridContainer } from '../../components/Grid';
 import ItemCard from './ItemCard';
 import WishListMenu from './WishListMenu';
 import WishListModal from './WishListModal';
+import { get, post, remove } from '../../utils/mockReqests';
+import { useCompound } from '../../hooks/common';
 
 
 const PageWrapper = styled.div`
@@ -34,28 +37,72 @@ const CardsWrapper = styled(GridContainer)`
   }
 `;
 
+
+
+const initialState = {
+  results: null,
+  wishlist: null,
+  resultStatus: 'initial',
+  wishlistStatus: 'initial',
+  showWishList: false,
+}
 const SearchResults = props => {
-  const [showWishList, setWishListState] = useState(false);
+  const { state, setState } = useCompound(initialState);
+  const openWishListModal = () => setState({ showWishList: true });
+  const closeWishListModal = () => setState({ showWishList: false });
 
+  const {
+    results,
+    wishlist,
+    resultStatus,
+    wishlistStatus,
+    showWishList,
+  } = state;
 
+  // we can dry this code up...
+  const updateSearchResults = async () => {
+    setState({ resultStatus: 'loading' });
+    const { data } = await get('/search/items', { query: '' });
+    if (data) setState({ results: data, resultStatus: 'loaded' });
+  }
+  const updateWishlist = async () => {
+    setState({ wishlistStatus: 'loading' });
+    const { data } = await get('/search/wishlist');
+    if (data) setState({ wishlist: data, wishlistStatus: 'loaded' });
+  }
+  const addToWishlist = async body => {
+    const { data } = await post('/wishlist', { ...body });
+    if (data) setState({ wishlist: data });
+  }
+  const removeFromWishlist = async body => {
+    const { data } = await remove('/wishlist', { ...body });
+    if (data) setState({ wishlist: data });
+  }
+  if (resultStatus === 'initial') updateSearchResults();
+  if (wishlistStatus === 'initial') updateWishlist();
   return (
     <PageWrapper>
       <CardsWrapper cols="4" gap="2vw">
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+        {resultStatus !== 'loaded' ? 
+          'Loading...'
+          :
+          results.map((res, i) =>
+            <ItemCard
+              key={`itemcard-${i+2}`}
+              {...res}
+              wishlist={wishlist}
+              addToWishlist={addToWishlist}
+              removeFromWishlist={removeFromWishlist}
+            />
+          )
+        }
       </CardsWrapper>
-      <WishListMenu />
+      <WishListMenu wishlist={wishlist} status={wishlistStatus} openWishListModal={openWishListModal} />
       <WishListModal
+        wishlist={wishlist}
         show={showWishList}
-        onOpen={() => setWishListState(true)}
-        onClose={() => setWishListState(false)}
+        onOpen={openWishListModal}
+        onClose={closeWishListModal}
       />
     </PageWrapper>
   )
