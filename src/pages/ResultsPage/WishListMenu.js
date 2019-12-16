c2af0596786d5a9f7aa6788b34f72ac827a3bf60
Frank Valdez <fvaldez421@gmail.com>
2019-12-16 02:29:20 -0800

    updated and completed styles, made the item count dynamic

--- src/pages/ResultsPage/WishListMenu.js
+++ src/pages/ResultsPage/WishListMenu.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 import SnackBar from '../../components/SnackBarWrapper';
+import { wishlistNotificationBg } from '../../components/Colors';
 
 
 const SnackBarContent = styled(SnackBar)`
-
+  position: sticky;
+  bottom: 0;
+  height: 60px;
+  border: 1px solid #000;
+  border-radius: 4px;
+  background-color: ${wishlistNotificationBg};
+  text-align: center;
+  cursor: pointer;
 `;
 
-const WishListMenu = props => {
+const MenuText = styled.p`
+  line-height: 60px;
+  font-weight: 600;
+  font-size: 16px;
+  user-select: none;
+`;
+
+const WishListMenu = ({ status='initial', wishlist=null, openWishListModal }) => {
+  const itemCount = wishlist ? Object.entries(wishlist).length : '';
   return (
-    <SnackBarContent>
+    <SnackBarContent show={true} onClick={openWishListModal}>
+      <MenuText>
+        {status !== 'loaded' ?
+          'Loading...'
+          :
+          `${itemCount} products in Wishlist`
+        }
+      </MenuText>
     </SnackBarContent>
   )
 };
