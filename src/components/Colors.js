39789ef7c644548596af354bf41f51f4fbafe397
Frank Valdez <fvaldez421@gmail.com>
2019-12-15 23:16:58 -0800

    added wishlist button colors

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,3 +1,7 @@
 export const lightButtonGrey = '#DDDDDD';
 export const hoverButtonGrey = '#CCCCCC';
 export const activeButtonGrey = '#BBBBBB';
+export const wishlistHeartRed = '#ff0000';
+export const wishlistHeartHoverFilled = '#e00000';
+export const wishlistHeartActiveFilled = '#bf0000';
+
