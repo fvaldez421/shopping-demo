ef5519816254f8ded45a161d5b438134d136d281
Frank Valdez <fvaldez421@gmail.com>
2019-12-16 23:09:25 -0800

    updated base button styles and added exit button

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,48 +1,88 @@
 import React from 'react';
 import styled from 'styled-components';
-import { lightButtonGrey, hoverButtonGrey, activeButtonGrey, wishlistHeartActiveFilled, wishlistHeartHoverFilled } from './Colors';
+import {
+  lightButtonGrey,
+  hoverButtonGrey,
+  activeButtonGrey,
+  wishlistHeartActiveFilled,
+  wishlistHeartHoverFilled
+} from './Colors';
 import AngleLeft from '../components/Icons/AngleLeft';
 import AngleRight from '../components/Icons/AngleRight';
 import Heart from '../components/Icons/Heart';
 
+
 export const BaseButton = styled.button`
   cursor: pointer;
+  border: none;
+  border-radius: 4px;
+  background-color: ${lightButtonGrey};
+  :hover {
+    background-color: ${hoverButtonGrey};
+  }
+  :active {
+    background-color: ${activeButtonGrey};
+  }
   &:active,
   &:focus,
   &:focus:active {
     outline: none;
   }
+  /* unset the focus dotted border on firefox */
+  ::-moz-focus-inner {
+    border: 0;
+  }
 `;
 
+export const ExitButton = styled(BaseButton)`
+  position: relative;
+  display: block;
+  padding: 0;
+  height: 26px;
+  min-width: 26px;
+  &::after {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    content: '+';
+    font-size: 30px;
+    line-height: 27px;
+    text-align: center;
+    transform: rotate(45deg);
+    /* sizing seems to behave differently in firefox */
+    @-moz-document url-prefix() {
+      font-size: 26px;
+      line-height: 22px;
+    }
+  }
+`;
+
+/**
+ * @param {String} [props.hoverColor] optional hover color
+ * @param {String} [props.activeColor] optional active color
+ */
 export const SvgBtnWrapper = styled(BaseButton)`
   background-color: unset;
-  border: none;
   /* The line below prevents picking up the svg and its children
      if an event handler is fired */
   > svg {
     pointer-events: none;
   }
   &:hover {
+    background-color: unset;
     > svg path {
       fill: ${({ hoverColor='' }) => hoverColor};
     }
   }
   &:active {
+    background-color: unset;
     > svg path {
       fill: ${({ activeColor='' }) => activeColor};
     }
   }
 `;
-
-export const WishlistBtn = ({ id='', filled=false, onClick }) => {
-  const hoverColor = filled ? wishlistHeartHoverFilled : hoverButtonGrey;
-  const activeColor = filled ? wishlistHeartActiveFilled : activeButtonGrey;
-  return (
-    <SvgBtnWrapper id={id} onClick={onClick} hoverColor={hoverColor} activeColor={activeColor}>
-      <Heart filled={filled} hoverColor="#000" />
-    </SvgBtnWrapper>
-  )
-}
 
 /**
  * Directional image navigation button
@@ -58,13 +98,6 @@ export const ImgNavBtn = styled(BaseButton)`
   border-bottom-left-radius: ${({ toLeft = false }) => toLeft ? '0' : '4px'};
   border-top-right-radius: ${({ toRight = false }) => toRight ? '0' : '4px'};
   border-bottom-right-radius: ${({ toRight = false }) => toRight ? '0' : '4px'};
-  background-color: ${lightButtonGrey};
-  :hover {
-    background-color: ${hoverButtonGrey};
-  }
-  :active {
-    background-color: ${activeButtonGrey};
-  }
   > svg {
     height: 16px;
     width: 16px;
@@ -72,12 +105,28 @@ export const ImgNavBtn = styled(BaseButton)`
   }
 `;
 
+/**
+ * @param {{ id: String, filled: Boolean, onClick: Function }} props
+ */
+export const WishlistBtn = ({ id='', filled=false, onClick }) => {
+  const hoverColor = filled ? wishlistHeartHoverFilled : hoverButtonGrey;
+  const activeColor = filled ? wishlistHeartActiveFilled : activeButtonGrey;
+  return (
+    <SvgBtnWrapper id={id} onClick={onClick} hoverColor={hoverColor} activeColor={activeColor}>
+      <Heart filled={filled} hoverColor="#000" />
+    </SvgBtnWrapper>
+  );
+};
 
+/**
+ * 
+ * @param {{ toRight: Boolean }} props 
+ */
 export const GalleryNavBtn = props => (
   <ImgNavBtn {...props}>
     {props.toRight ?
       <AngleRight /> : <AngleLeft />
     }
   </ImgNavBtn>
-)
+);
 
