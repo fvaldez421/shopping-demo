7cc43576ee884818593255b32fedcdefdc36e1e5
Frank Valdez <fvaldez421@gmail.com>
2019-12-16 23:10:32 -0800

    added styles to contain modal content instykled whtie box

--- src/components/ModalWrapper.js
+++ src/components/ModalWrapper.js
@@ -13,15 +13,43 @@ const ModalDialogue = styled.div`
   background-color: rgba(0,0,0,.5);
 `;
 
+const ModalContentContainer = styled.div`
+  box-sizing: border-box;
+  margin: 20vh auto;
+  padding: 30px;
+  min-height: 400px;
+  min-width: 400px;
+  height: 45vh;
+  width: 55vw;
+  max-width: 600px;
+  border-radius: 4px;
+  background-color: #fff;
+  box-shadow: 0px 1px 6px #000;
+  @media only screen and (max-width: 450px) {
+    margin-top: 10vh;
+    max-width: 95vw;
+    width: 95vw;
+    min-width: unset;
+  }
+`;
+
+/**
+ * Reuseable Modal component. Appends a node in the dom 
+ * when it is imported and then renders from within
+ * @param {{ id: [String], show: Boolean, autoClose: Boolean, onClose: Function, onOpen: Function, children: Function, ...rest: Object }} 
+ */
 const ModalWrapper = ({ id='Modal', show, autoClose, onClose, onOpen, children, ...props }) => {
-  function handleBgClick() {
+  const handleBgClick = () => {
     if (autoClose) onClose();
   }
+  const killEvent = e => e.stopPropagation();
   return (
     <Portal id="ModalPortal" className="modal-parent">
       {show ?
         <ModalDialogue onClick={handleBgClick} {...props}>
-          {show ? children : ''}
+          <ModalContentContainer onClick={killEvent}>
+            {children}
+          </ModalContentContainer>
         </ModalDialogue>
         : ''
       }
